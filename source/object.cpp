c461c17b80e35a357f1dfa995d125d108bc6e582
James Butcher <jamesbutcher167@gmail.com>
2021-09-28 22:27:59 +0100

    Add class method implementation

--- source/object.cpp
+++ source/object.cpp
@@ -2,9 +2,12 @@
 #include "object.hpp"
 
 Square::Square(int x, int y, int width, int height){
-
+    this->x = x;
+    this->y = y;
+    this->width = width;
+    this->height = height;
 }
 
 void Square::draw() const{
-
+    DrawRectangle(x, y, width, height, BLUE);
 }
