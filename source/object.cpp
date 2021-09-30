8a67ce4f0f46978e43673be4a40034f0ffc8e970
James Butcher <jamesbutcher167@gmail.com>
2021-10-01 00:49:23 +0100

    Add drawing for tetris objects

--- source/object.cpp
+++ source/object.cpp
@@ -1,5 +1,9 @@
 #include <raylib.h>
+#include <vector>
 #include "object.hpp"
+#include <iostream>
+
+#define LOG(x) std::cout << x << std::endl
 
 Square::Square(int x, int y, int width, int height){
     this->x = x;
@@ -10,4 +14,22 @@ Square::Square(int x, int y, int width, int height){
 
 void Square::draw() const{
     DrawRectangle(x, y, width, height, BLUE);
+}
+
+
+TetrisBlock::TetrisBlock(int x, int y, int shape){
+  this->x = x;
+  this->y = y;
+}
+
+void TetrisBlock::draw() const{
+  for (int i = 0; i < 25; i++){
+    if (shape[i]){
+      int deltaX = i%5;
+      int deltaY = (int)i/5;
+
+      DrawRectangle(x+(deltaX*10), y+(deltaY*10), 10, 10, BLUE);
+
+    }
+  }
 }
