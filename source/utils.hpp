eff95a7b0740baf8380bdf2a3c3e986c50ba2b52
James Butcher <jamesbutcher167@gmail.com>
2021-10-01 20:40:23 +0100

    Improve safeGet function

--- source/utils.hpp
+++ source/utils.hpp
@@ -2,7 +2,7 @@
 
 template<class T>
 T safeGet(int index, T* data, int length, T base = 0){
-    if (index < length)
+    if (index < length && index >= 0)
         return *(data+index);
     return base;
 }
