45241d1d26069212af358cc19fe8cfbd1a0e71f4
James Butcher <jamesbutcher167@gmail.com>
2021-10-07 22:43:44 +0100

    Add int round function

--- source/utils.hpp
+++ source/utils.hpp
@@ -4,7 +4,12 @@
 
 template<class T, int L>
 T safeGet(int index, std::array<T, L> data, T base = 0){
-    if (index < L && index >= 0)
-        return data[index];
-    return base;
+  if (index < L && index >= 0)
+    return data[index];
+  return base;
+}
+
+template<class T>
+int round(T x){
+  return (int)(x + .5);
 }
