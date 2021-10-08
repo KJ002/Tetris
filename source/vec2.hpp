7921dd7f593a0800dd7b05fa3573f926db332922
James Butcher <jamesbutcher167@gmail.com>
2021-10-08 01:15:40 +0100

    Fix non 0 rotational origin not being added back

--- source/vec2.hpp
+++ source/vec2.hpp
@@ -80,7 +80,7 @@ struct Vec2 : public Vector2{
   Vec2 rotate(const double th) const{
     Vec2 result;
 
-    float rad = degToRad(th);
+    float rad = degToRad(360-th);
 
     result.x = std::cos(rad) * this->x - std::sin(rad) * this->y;
     result.y = std::sin(rad) * this->x + std::cos(rad) * this->y;
@@ -91,7 +91,7 @@ struct Vec2 : public Vector2{
   Vec2 rotate(const double th, const Vec2& origin) const{
     Vec2 newPoint = *this - origin;
 
-    return newPoint.rotate(th);
+    return newPoint.rotate(th)+origin;
   }
 
   float length() const{
