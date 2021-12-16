6988c1d34ecc8d4e1b36240e0705a5b9b0d6e3e7
James Butcher <jamesbutcher167@gmail.com>
2021-12-16 23:05:00 +0000

    Update getCoords symbol comments

--- source/asm_utils.asm
+++ source/asm_utils.asm
@@ -7,15 +7,18 @@ getStack:
     pop rax
     ret
 
-;; RDI - grid width (modulo)
-;; RSI - grid height (floor division)
+;; RDI - value
+;; RSI - divisor
+;; Values are pushed onto stack
+;; rdx (modulo) is pushed first then
+;; rax (floor division) is pushed second
 getCoords:
     mov rdx, 0
     mov rax, rdi
     mov rcx, rsi
     div rcx
 
-    push rax
     push rdx
+    push rax
 
     ret
