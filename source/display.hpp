c0efa34664fcda0315eb31ea2404528606792a11
James Butcher <jamesbutcher167@gmail.com>
2021-12-23 12:38:32 +0000

    Add differantiation between dynamic and static objects

--- source/display.hpp
+++ source/display.hpp
@@ -9,8 +9,12 @@ class Display{
 private:
   int screenWidth, screenHeight;
   std::string profile;
-  std::vector<Object*>* profileRef = nullptr;
-  std::map<std::string, std::vector<Object*>> profiles;
+
+  std::vector<Object*>* dynamicProfileRef = nullptr;
+  std::vector<Object>* staticProfileRef = nullptr;
+
+  std::map<std::string, std::vector<Object*>> dynamicProfiles;
+  std::map<std::string, std::vector<Object>> staticProfiles;
 
   Vector2 WtoS(Vector2);
 
@@ -18,10 +22,17 @@ public:
   Display(int width, int height, const char* title, int fps, std::string profile = "main");
   void switchProfile(std::string profile);
   std::string getProfile() {return this->profile;};
+
   void attachShape(Object* shape);
   void removeShape(Object* shape);
+
+  void attachShape(Object shape);
+  void removeShape(Object shape);
+
+  void clearDynamic();
+  void clearStatic();
+
   void drawShape();
   void closeScreen() const;
-  void clear();
   std::vector<Object*> exposeShapes(std::string profile) const;
 };
